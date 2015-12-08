0acbcac7ad1d502ee56da715a1b3c08b2dfbaff7
Aivean <aiveeen@gmail.com>
2015-12-07 21:16:20 -0800

    fix #8 Make path calculation asynchronous

--- src/main/scala/com/aivean/isorpg/game/World.scala
+++ src/main/scala/com/aivean/isorpg/game/World.scala
@@ -1,5 +1,7 @@
 package com.aivean.isorpg.game
 
+import java.util.concurrent.ForkJoinPool
+
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.event.LoggingReceive
 import com.aivean.isorpg.game.Tiles.TileType
@@ -9,6 +11,7 @@ import xitrum.Config
 
 import scala.collection.immutable.ListMap
 import scala.collection.mutable
+import scala.concurrent.{Future, ExecutionContext}
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import scala.util.Random
@@ -21,15 +24,14 @@ import scala.util.Random
 
   class World private extends Actor with ActorLogging {
     import World._
+    import akka.pattern.pipe
     implicit val ec = context.dispatcher
 
     val visContr = context.actorOf(VisibilityController.props, "visContrl")
 
     val worldSize = 100
-    var world =   TerrainGen.apply(worldSize)
-    var chunks = world.groupBy(_._1.chunk)
+    var world = new Terrain(TerrainGen.apply(worldSize), Set())
 
-    var occupied = Set[Point]()
     var players = Map[ActorRef, CharView]()
     var npcs = Map[ActorRef, CharView]()
     var indexByUuid = Map[String, CharView]()
@@ -38,7 +40,7 @@ import scala.util.Random
 
     for(i <- 1 to 100) {
         val fp = genFreePoint
-        occupied += fp
+        world = world.addOccupant(fp)
         val uuid = Utils.uuid
         val a = context.actorOf(Monster.props(fp), "poring-" + uuid)
         val view = new CharView(uuid, CharState.Standing(fp), a, "poring")
@@ -47,17 +49,11 @@ import scala.util.Random
         visContr ! VisibilityController.NPCAdded(CharCommons(view.uuid, view.state.p, view.sprite))
       }
 
-    def freeToMoveTo(p:Point) =
-      world.get(p.down).exists(_.standable) &&
-      !world.get(p).exists(!_.passable) &&
-      !occupied.contains(p)
-
-    def zShift(p:Point) = world.get(p.down).map(_.height - 1f).getOrElse(0f)
+    def zShift(p:Point) = world.map.get(p.down).map(_.height - 1f).getOrElse(0f)
 
     def genFreePoint = {
-      val p = Random.shuffle(players.map(_._2.state.p)).headOption.getOrElse(Random.shuffle(world.keys.toSeq).head)
-
-      Random.shuffle(world.keys.toSeq.filter(_.distTo(p) <= 10).map(_.up).filter(freeToMoveTo)).head
+      val p = Random.shuffle(players.map(_._2.state.p)).headOption.getOrElse(Random.shuffle(world.map.keys.toSeq).head)
+      Random.shuffle(world.map.keys.toSeq.filter(_.distTo(p) <= 10).map(_.up).filter(world.freeToMoveTo)).head
     }
 
     def receive = LoggingReceive {
@@ -68,7 +64,7 @@ import scala.util.Random
           val actorRef = context.actorOf(Player.props(pos, client), "player-" + uuid)
           new CharView(uuid, CharState.Standing(pos), actorRef, "player")
         }
-        occupied += char.state.p
+        world = world.addOccupant(char.state.p)
         players += (char.actor -> char)
         indexByUuid += (char.uuid -> char)
 
@@ -79,8 +75,8 @@ import scala.util.Random
         val NEIGHTBOURS_PATTERN = """tileNeighbors[^\d-]+(-?[\d]+)[^\d-]+(-?[\d]+)[^\d-]+(-?[\d]+)""".r
         cmd match {
         case "generateWorld" =>
-          world = TerrainGen.apply(worldSize)
-          chunks = world.groupBy(_._1.chunk)
+          //FIXME broken! need to reposition chars correctly
+          world = new Terrain(TerrainGen.apply(worldSize), world.occupied)
           players.foreach(_._2.actor ! Player.ServerMessage("World Regenerated!"))
         case "location" =>
           players.get(player) foreach { player =>
@@ -92,7 +88,7 @@ import scala.util.Random
             val result = ListMap("point" -> p, "s" -> p.south, "sw" -> p.south.west, "w" -> p.west, "nw" -> p.north
               .west, "n" -> p.north, "ne" -> p.north.east, "e" -> p.east, "se" -> p.south.east, "up" -> p.up,
               "down" -> p.down)
-            .mapValues(world.get(_).map(_.tile).getOrElse("")).map(x => x._1 + ":"+x._2).mkString(", ")
+            .mapValues(world.map.get(_).map(_.tile).getOrElse("")).map(x => x._1 + ":"+x._2).mkString(", ")
             player ! Player.ServerMessage(result)
           } catch {case e: Throwable => player ! Player.ServerMessage("Error: " + e.getMessage)}
 
@@ -106,35 +102,41 @@ import scala.util.Random
 
             log.debug(s"found player $movingPlayer ${movingPlayer.state}")
 
-            val path = {
-              val startP = movingPlayer.state.p
-              def calcPath(pts:Set[Point]) = Utils.path(startP, pts, { (p, p1) => freeToMoveTo(p1) })
-              calcPath(Set(targetP)).orElse(targetP.closeProximity.toSet match {
-                case x if !x.contains(startP) => x.filter(freeToMoveTo) match {
-                  case Seq() => None
-                  case x => calcPath(x)
-                }
-                case _ => None
-              })
-            }
-
-            log.debug(s"found path $path")
-
-            path.flatMap(_.slice(1, 2).headOption).foreach { nextPoint =>
-              log.debug(s"moving to next point $nextPoint")
-              val stepTime = 400 milliseconds
-
-              val ts = System.currentTimeMillis() + stepTime.toMillis
-              occupied -= movingPlayer.state.p
-              movingPlayer.state = CharState.MovingTo(nextPoint, ts)
-              occupied += movingPlayer.state.p
-
-              visContr ! VisibilityController.CharMoving(movingPlayer.uuid, ts, nextPoint, zShift(nextPoint))
-
-              context.system.scheduler.scheduleOnce(stepTime, self,
-                UpdateCharState(movingPlayer.actor))
-            }
+            val world = this.world //closing world
+            Future {
+              { // path
+                val startP = movingPlayer.state.p
+                def calcPath(pts: Set[Point]) = Utils.path(startP, pts, { (p, p1) => world.freeToMoveTo(p1) })
+                calcPath(Set(targetP)).orElse(targetP.closeProximity.toSet match {
+                  case x if !x.contains(startP) => x.filter(world.freeToMoveTo) match {
+                    case Seq() => None
+                    case x => calcPath(x)
+                  }
+                  case _ => None
+                })
+              }.foreach { path =>
+                  log.debug(s"found path $path")
+                  self ! CharAdvance(movingPlayer.actor, targetP, path)
+              }
+            } (pathCalculationEc)
           }
+
+      case CharAdvance(charActor, targetP, path) =>
+        findChar(charActor).foreach { movingChar =>
+          path.slice(1, 2).headOption.filter(world.freeToMoveTo).foreach { nextPoint =>
+            log.debug(s"moving to next point $nextPoint")
+            val stepTime = 400 milliseconds
+
+            val ts = System.currentTimeMillis() + stepTime.toMillis
+            world = world.moveOccupant(movingChar.state.p, nextPoint)
+            movingChar.state = CharState.MovingTo(nextPoint, ts)
+
+            visContr ! VisibilityController.CharMoving(movingChar.uuid, ts, nextPoint, zShift(nextPoint))
+
+            context.system.scheduler.scheduleOnce(stepTime, self,
+              UpdateCharState(movingChar.actor))
+          }
+        }
 
       case PlayerWantsToAttack(uuid) => findChar(sender)
         .filter(_.state.isInstanceOf[CharState.Standing]).foreach { player =>
@@ -167,9 +169,7 @@ import scala.util.Random
               context.system.scheduler.scheduleOnce((x.endTs - time) milliseconds, self, msg)
 
             case CharState.MovingTo(p, ts) =>
-              occupied -= player.state.p
               player.state = CharState.Standing(p)
-              occupied += p
               log.debug(s"arrived at $p")
               player.actor ! StatePolling.ArrivedAt(p)
               visContr ! VisibilityController.CharPositionChanged(player.uuid, p)
@@ -184,20 +184,23 @@ import scala.util.Random
 
       case ChunksRequest(chIds) =>
         sender ! PlayerChunksController.TilesAdded(
-          chIds.toList.flatMap(id => chunks.get(id).map(c => (id, c))).toMap)
+          chIds.toList.flatMap(id => world.chunks.get(id).map(c => (id, c))).toMap)
 
       case SurroundingsRequest(point, dist) =>
-        val q = mutable.Queue[Point](point)
-        var res = Map[Point, TileType]()
-        while (q.nonEmpty) {
-          val p = q.dequeue()
-          val toAdd = (p.adjFlatAll :+ p.up :+ p.down)
-            .filterNot(res.contains).filter(_.distTo(point) <= dist)
-            .flatMap(p => world.get(p).map(t => (p, t))).toMap
-          res ++= toAdd
-          toAdd.keys.foreach(q.enqueue(_))
-        }
-        sender ! Monster.SurroundingsView(res)
+        val world = this.world
+        Future {
+          val q = mutable.Queue[Point](point)
+          var res = Map[Point, TileType]()
+          while (q.nonEmpty) {
+            val p = q.dequeue()
+            val toAdd = (p.adjFlatAll :+ p.up :+ p.down)
+              .filterNot(res.contains).filter(_.distTo(point) <= dist)
+              .flatMap(p => world.map.get(p).map(t => (p, t))).toMap
+            res ++= toAdd
+            toAdd.keys.foreach(q.enqueue(_))
+          }
+          Monster.SurroundingsView(res)
+        }(pathCalculationEc) pipeTo sender
 
       //TODO delegate broadcast functionality to other dedicated actor
 
@@ -209,8 +212,8 @@ import scala.util.Random
         players.get(sender).foreach { p =>
           players -= p.actor
           indexByUuid -= p.uuid
-          occupied -= p.state.p
-          visContr ! VisibilityController.CharRemoved(p.uuid)
+          world = world.removeOccupant(p.state.p)
+          visContr !VisibilityController.CharRemoved(p.uuid)
         }
     }
   }
@@ -219,8 +222,14 @@ import scala.util.Random
     //def props = Props(new World)
     val world = Config.actorSystem.actorOf(Props(new World), "world")
 
+    private val pathCalculationEc = ExecutionContext.fromExecutorService(new ForkJoinPool(8))
+
+    // private messages
     private case class UpdateCharState(player:ActorRef)
 
+    private case class CharAdvance(player:ActorRef, targetP: Point, path:List[Point])
+
+    // public messages
     case class ClientConnected(uuid: String)
 
     case object ClientDisconnected
@@ -236,6 +245,23 @@ import scala.util.Random
     case class ChunksRequest(ids:Set[Long])
 
     case class SurroundingsRequest(p:Point, dist:Float)
+
+    class Terrain private (val map:Map[Point, TileType], val occupied:Set[Point],
+                           val chunks:Map[Long, Map[Point,TileType]]) {
+      def this(map: Map[Point, TileType], occupied: Set[Point]) = this(map, occupied, map.groupBy(_._1.chunk))
+
+      //private def changeMap()
+      private def changeOccupancy(func:Set[Point] => Set[Point]) = new Terrain(map, func(occupied), chunks)
+
+      def moveOccupant(from:Point, to:Point) = changeOccupancy(_ - from + to)
+      def addOccupant(to:Point) = changeOccupancy(_ + to)
+      def removeOccupant(from:Point) = changeOccupancy(_ - from)
+
+      def freeToMoveTo(p:Point) =
+        map.get(p.down).exists(_.standable) &&
+          !map.get(p).exists(!_.passable) &&
+          !occupied.contains(p)
+    }
 
     class CharView(
                     val uuid: String,
